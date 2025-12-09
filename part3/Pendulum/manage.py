82d29ab3368e98f42c94a66f3f9423ea9ae1edc5
Paige0909 <peichun0909@gmail.com>
2025-12-09 13:50:52 +0800

    Update manage.py

--- part3/Pendulum/manage.py
+++ part3/Pendulum/manage.py
@@ -60,41 +60,6 @@ class PendulumEnvWrapper:
         """
         return self.current_observation
 
-#-----Agent abstract base class: Inheritance + Polymorphism-----
-class Agent(ABC):
-    """
-    Agent base class(abstract class)
-    *Every concrete Agent should inherit from this class and implement act() and reset()
-    *Also realize polymorphism
-    """
-    def __init__(self, action: int, max_action: float):
-        self.action=action
-        self.max_action=max_action
-
-    @abstractmethod
-    def act(self, observation: np.ndarray)->np.ndarray:
-        """
-        Return action according to observation(need overwritten)
-        """
-        raise NotImplementedError
-    
-    @abstractmethod
-    def reset(self):
-        """
-        Initialize when episode start
-        """
-        raise NotImplementedError
-    
-class RandomAgent(Agent):
-    """
-    A random agent that output random actions within the allowed range
-    """
-    def act(self, observation: np.ndarray)->np.ndarray:
-        return np.random.uniform(-self.max_action, self.max_action, size=self.action)
-    
-    def reset(self):
-        pass
-
 #------Environment Management+ Agent Management-----
 class Experiment:
     """
