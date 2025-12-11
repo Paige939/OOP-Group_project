38d27d33a8a79a6bc501b581ad9b75a90d7f9fc3
boLavender <barryhuang1119@gmail.com>
2025-12-12 00:52:41 +0800

    Update __init__.py

--- part3/Pendulum/Agents/__init__.py
+++ part3/Pendulum/Agents/__init__.py
@@ -1,4 +1,7 @@
 from .random_agent import RandomAgent
 from .CEM_agent import CEM_Agent
+from .energy_agent import EnergyControlAgent
+from .lqr_agent import LQRAgent
+from .energy_lqr_agent import ELAgent
 
-__all__=["RandomAgent", "CEM_Agent"]
+__all__=["RandomAgent", "CEM_Agent", "EnergyControlAgent", "LQRAgent", "ELgent"]
